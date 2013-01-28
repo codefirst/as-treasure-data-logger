dca85e226aa61dcd6d3229a6afb4d2408030e349
mallowlabs <mallowlabs@gmail.com>
2013-01-28 23:56:04 +0900

    Changed: loop

--- lib/main.rb
+++ lib/main.rb
@@ -16,25 +16,34 @@ COUNT = 100
 uri = URI.parse(REDIS_URL)
 redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
-since_id = redis.get("since_id")
+loop do
 
-url = "#{ASAKUSA_SATELLITE_ENTRY_POINT}/message/list.json"
-url += "?api_key=#{ASAKUSA_SATELLITE_API_KEY}"
-url += "&room_id=#{ASAKUSA_SATELLITE_ROOM_ID}"
-url += "&count=#{COUNT}"
-url += "&since_id=#{since_id}" if since_id
+  since_id = redis.get("since_id")
 
-messages = []
-open(url) do |f|
-  messages = JSON.load(f).map { |m| m["view"] = nil; m }
+  url = "#{ASAKUSA_SATELLITE_ENTRY_POINT}/message/list.json"
+  url += "?api_key=#{ASAKUSA_SATELLITE_API_KEY}"
+  url += "&room_id=#{ASAKUSA_SATELLITE_ROOM_ID}"
+  url += "&count=#{COUNT}"
+  url += "&since_id=#{since_id}" if since_id
+
+  messages = []
+  begin
+    open(url) do |f|
+      messages = JSON.load(f).map { |m| m["view"] = nil; m }
+    end
+  rescue => e
+    warn e
+  end
+
+  messages.shift
+
+  messages.each do |m|
+    puts "[@heroku.message] " + m.to_json.gsub("\n", "")
+    since_id = m["id"]
+  end
+
+  redis.set("since_id", since_id)
+
+  sleep 600
+
 end
-
-messages.shift
-
-messages.each do |m|
-  puts "[@heroku.message] " + m.to_json.gsub("\n", "")
-  since_id = m["id"]
-end
-
-redis.set("since_id", since_id)
-
